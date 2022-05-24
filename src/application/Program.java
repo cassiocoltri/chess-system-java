7b1fa6dd818351a80b6ac7557869471a12d16b26
cassiocoltri <cassiocoltri@gmail.com>
2022-05-24 17:50:38 -0300

    Starting to implement Board and Piece

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,15 +1,13 @@
 package application;
 
-import boardgame.Position;
+import boardgame.Board;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Position p1 = new Position(10, 20);
+		Board board = new Board(8, 8);
 		
-		System.out.println(p1);
-
 	}
 
 }
---
+++ src/boardgame/Board.java
@@ -0,0 +1,37 @@
+package boardgame;
+
+public class Board {
+	
+	private int rows;
+	private int columns;
+	
+	// Matriz para o tabuleiro.
+	private Piece[][] pieces;
+
+	public Board(int rows, int columns) {
+		this.rows = rows;
+		this.columns = columns;
+		pieces = new Piece[rows][columns];
+		// O construtor 'board' vai já criar o "grid" no tamanho das linhas/colunas informadas
+	}
+
+	public int getRows() {
+		return rows;
+	}
+
+	public void setRows(int rows) {
+		this.rows = rows;
+	}
+
+	public int getColumns() {
+		return columns;
+	}
+
+	public void setColumns(int columns) {
+		this.columns = columns;
+	}
+	
+	
+	
+
+}
