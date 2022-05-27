6dcc84696fc82a69ef756209cab511bcf988a3ab
cassiocoltri <cassiocoltri@gmail.com>
2022-05-27 16:57:24 -0300

    Possible moves of a piece

--- src/boardgame/Piece.java
+++ src/boardgame/Piece.java
@@ -1,6 +1,6 @@
 package boardgame;
 
-public class Piece {
+public abstract class Piece {
 	
 	protected Position position;
 	private Board board;
@@ -13,6 +13,29 @@ public class Piece {
 
 	protected Board getBoard() {
 		return board;
+	}
+	
+	public abstract boolean[][] possibleMoves();  // <-- Obrigatorio a implementar!
+	
+	/* Metodo Hook (gancho)
+	Como é obrigatório as classes de Piece configurar o "possibleMoves", não tem problema deixar o metodo já "concreto"
+	pois temos a garantia que ele sera usado.
+	*/ 
+	
+	public boolean possibleMove(Position position) {
+		return possibleMoves()[position.getRow()][position.getColumn()];
+	}
+	
+	public boolean isThereAnyPossibleMove() {
+		boolean[][] mat = possibleMoves();
+		for (int i = 0; i < mat.length; i++) {
+			for (int j = 0; j < mat.length; j++) {
+				if (mat[i][j]) {
+					return true;
+				}
+			}
+		}
+		return false;
 	}
 	
 
